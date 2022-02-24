775ba1fb475864972c993fcba3e3cde6c4dff2e3
Leon Richli RICL2 <leon.richli@coop.ch>
2022-02-24 12:49:56 +0100

    Divere änderungenn am Code

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+import imp
+from urllib import request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json})
+    else:
+        return jsonify ({"about": "Hello World!!!"}) 
+
+@app.route('/<string:marke>/<string:model>/<string:jahr>', methods=['GET'])
+def data(marke, model, jahr):
+        return jsonify({'brand': marke, 'model': model, 'year': jahr})
+
+
+@app.route("/cars")
+def cars():
+    return jsonify({"brand": "Nissan", "model": "Skyline R34", "year": 2004})
